5a0dc8a3233321cb6ea4b36bba2534bf049f2b0d
bwhitmore7 <>
2021-04-07 22:53:58 +0000

    saving

--- Main.java
+++ Main.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    String[ ] firstNames = {"Bryson", "Dylan", "Abdulah", "jay", "connor", "chase", "shrek"};
+    String[] firstNames = { "Bryson", "Dylan", "Abdulah", "jay", "connor", "chase", "shrek" };
     Scanner scan = new Scanner(System.in);
-     System.out.println("What is your full name? " );
-     String fullName = scan.nextLine();
-
-     int index = fullName.indexOf(" "); 
-     String name = fullName.substring(0, index);
-
-     
-   }
+    System.out.println("What is your full name? ");
+    String fullName = scan.nextLine();
+    int index = fullName.indexOf("5");
+    String firstName = fullName.substring(0, 5);
+    System.out.println(" what is the middle characecter in your name?");
+    String index1 = scan.next();
+  }
 }
